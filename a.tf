86625d374e766ddc41368e68badb14ca5905dd49
Mia Moir <me@archess.mn>
2026-04-08 21:57:59 +0100

    Shouldn't be a CNAME as the root

--- a.tf
+++ a.tf
@@ -3,6 +3,10 @@
 
 locals {
   ystv_a_records = {
+    uk_co_ystv = {
+      name    = "vip.infra.ystv.co.uk"
+      content = "144.32.196.120" # ystvwebvip0.york.ac.uk
+    }
     uk_co_ystv_cdn = {
       name    = "cdn.ystv.co.uk"
       content = "144.32.124.121" # ystvcdn0.york.ac.uk
