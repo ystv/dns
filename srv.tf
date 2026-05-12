fc17199092950b4e4c8ef4e8b81229969086936d
Mia Moir <me@archess.mn>
2026-05-12 19:32:36 +0100

    DNS Caching my beloathed

--- srv.tf
+++ srv.tf
@@ -13,3 +13,19 @@ resource "cloudflare_dns_record" "record_srv_uk_co_ystv_elections_minecraft" {
   settings = {}
   comment  = var.dns_record_comment
 }
+
+resource "cloudflare_dns_record" "record_srv_uk_co_ystv_elections_minecraft_2" {
+  name    = "_minecraft._tcp.elect.ystv.co.uk"
+  proxied = false
+  ttl     = 1
+  type    = "SRV"
+  zone_id = var.ystv_co_uk_zone_id
+  data = {
+    port     = 25565
+    priority = 0
+    target   = "temjin.infra.archess.mn"
+    weight   = 0
+  }
+  settings = {}
+  comment  = var.dns_record_comment
+}
