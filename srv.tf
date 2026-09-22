4b336a9be78a72e6e89ca693d1967e58bd8d164b
Mia Moir <me@archess.mn>
2026-09-22 16:17:43 +0100

    Try and get SRV records to not be changed every run

--- srv.tf
+++ srv.tf
@@ -10,6 +10,7 @@ resource "cloudflare_dns_record" "record_srv_uk_co_ystv_elections_minecraft" {
     target   = "temjin.infra.archess.mn"
     weight   = 0
   }
+  tags = []
   settings = {}
   comment  = var.dns_record_comment
 }
@@ -26,6 +27,7 @@ resource "cloudflare_dns_record" "record_srv_uk_co_ystv_elections_minecraft_2" {
     target   = "temjin.infra.archess.mn"
     weight   = 0
   }
+  tags = []
   settings = {}
   comment  = var.dns_record_comment
 }
